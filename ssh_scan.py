aa6646c5e1f091f92b309bd7b2c74ea812b9edef
Nicolas Bonnet <nicolas.bonnet@telecom-sudparis.eu>
2018-12-20 19:46:58 +0100

    Add a timeout argument

--- ssh_scan.py
+++ ssh_scan.py
@@ -17,9 +17,9 @@ def get_logger():
     return logger
 
 
-def get_certificate(addr):
+def get_certificate(addr, timeout):
     logger.debug("Checking cert {}".format(addr))
-    with subprocess.Popen(["ssh-keyscan", "-t", "rsa", str(addr)],
+    with subprocess.Popen(["ssh-keyscan", "-t", "rsa", "-T", str(timeout), str(addr)],
                           stdout=subprocess.PIPE,
                           stderr=subprocess.DEVNULL) as proc:
         crt = proc.stdout.read()
@@ -37,8 +37,8 @@ def get_certificate(addr):
     return crt, addr
 
 
-def get_all_certs(executor, ip_range):
-    addr_cert = executor.map(get_certificate, ip_range)
+def get_all_certs(executor, ip_range, timeout):
+    addr_cert = executor.map(lambda addr: get_certificate(addr, timeout), ip_range)
     addr_cert = filter(lambda x: x[0], addr_cert)  # Remove hosts that have no certs
     return dict(addr_cert)
 
@@ -47,13 +47,16 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Scan for a host that has SSH enabled in two networks.')
     parser.add_argument('network1', type=str, action='store', help='network1')
     parser.add_argument('network2', type=str, action='store', help='network2')
+    parser.add_argument('--timeout', type=int, action='store', default=5,
+                        help='timeout expressed in seconds for each certificate request')
     args = parser.parse_args()
 
+    timeout = args.timeout
     logger = get_logger()
 
     with ThreadPoolExecutor(max_workers=64) as executor:
-        lan1_addr_cert = get_all_certs(executor, ip_network(args.network1))
-        lan2_addr_cert = get_all_certs(executor, ip_network(args.network2))
+        lan1_addr_cert = get_all_certs(executor, ip_network(args.network1), timeout)
+        lan2_addr_cert = get_all_certs(executor, ip_network(args.network2), timeout)
 
     certs_in_both_networks = set(lan1_addr_cert.keys()) & set(lan2_addr_cert.keys())
     for cert in certs_in_both_networks:
