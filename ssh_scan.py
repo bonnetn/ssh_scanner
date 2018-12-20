23785098eb16e113e24dc22b086798600f0bc692
Nicolas Bonnet <nicolas.bonnet@telecom-sudparis.eu>
2018-12-20 18:58:20 +0100

    Rename LOG -> logger

--- ssh_scan.py
+++ ssh_scan.py
@@ -8,31 +8,31 @@ from ipaddress import ip_network
 
 
 def get_logger():
-    LOG = logging.getLogger("ssh_scanner")
-    LOG.setLevel(logging.INFO)
+    logger = logging.getLogger("ssh_scanner")
+    logger.setLevel(logging.INFO)
     handler = logging.StreamHandler()
     formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
     handler.setFormatter(formatter)
-    LOG.addHandler(handler)
-    return LOG
+    logger.addHandler(handler)
+    return logger
 
 
 def get_certificate(addr):
-    LOG.debug("Checking cert {}".format(addr))
+    logger.debug("Checking cert {}".format(addr))
     with subprocess.Popen(["ssh-keyscan", "-t", "rsa", str(addr)],
                           stdout=subprocess.PIPE,
                           stderr=subprocess.DEVNULL) as proc:
         crt = proc.stdout.read()
 
     if not crt:
-        LOG.debug("No certificate found for {}".format(addr))
         return addr, None
+        logger.debug("No certificate found for {}".format(addr))
 
     crt = crt.split()
     crt = crt[-1]
     crt = crt.decode()
 
-    LOG.debug("Got certificate for {}".format(addr))
+    logger.debug("Got certificate for {}".format(addr))
 
     return crt, addr
 
@@ -49,7 +49,7 @@ if __name__ == "__main__":
     parser.add_argument('network2', type=str, action='store', help='network2')
     args = parser.parse_args()
 
-    LOG = get_logger()
+    logger = get_logger()
 
     with ThreadPoolExecutor(max_workers=64) as executor:
         lan1_addr_cert = get_all_certs(executor, ip_network(args.network1))
@@ -59,4 +59,4 @@ if __name__ == "__main__":
     for cert in certs_in_both_networks:
         addr1 = lan1_addr_cert[cert]
         addr2 = lan2_addr_cert[cert]
-        LOG.info("{} = {} is SSHable in both networks.".format(addr1, addr2))
+        logger.info("{} = {} is SSHable in both networks.".format(addr1, addr2))
